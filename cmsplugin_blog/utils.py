b783ab62e508a10de1dfba6013ecf7d4be584c9e
Jonathan Stoppani <jonathan@stoppani.name>
2014-01-02 12:43:07 +0100

    Update utils.py

--- cmsplugin_blog/utils.py
+++ cmsplugin_blog/utils.py
@@ -1,6 +1,16 @@
 from django.utils.translation import get_language, ugettext_lazy as _
 from cms import settings
-from cms.middleware.multilingual import has_lang_prefix
+
+
+HAS_LANG_PREFIX_RE = re.compile(r"^/(%s)/.*" % "|".join([re.escape(lang[0]) for lang in settings.CMS_LANGUAGES]))
+
+
+def has_lang_prefix(path):
+    check = HAS_LANG_PREFIX_RE.match(path)
+    if check is not None:
+        return check.group(1)
+    else:
+        return False
 
 def is_multilingual():
     return 'cmsplugin_blog.middleware.MultilingualBlogEntriesMiddleware' in settings.MIDDLEWARE_CLASSES
